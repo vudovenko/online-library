54a44bfaa98ce2755c77652000d505b221437f74
vudovenko <vladislav.udovenko0@yandex.ru>
2024-11-07 17:02:28 +0500

    git init

---
+++ src/test/java/dev/vudovenko/onlinelibrary/OnlineLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.onlinelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
